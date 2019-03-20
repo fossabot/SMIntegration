ba5ca8f5753d4018789e4690cee8f2c9a0202e37
ribeirojose <ribeirojose@me.com>
2019-03-20 15:55:34 -0300

    Add fetch_pages method

--- services/survey_processor.py
+++ services/survey_processor.py
@@ -3,16 +3,25 @@ import json
 import requests
 
 class SurveyProcessor:
-    def __init__(self, survey_id):
-        self.survey_endpoint = 'https://api.surveymonkey.com/v3/surveys/{}'.format(survey_id)
+    def __init__(self):
+        self.survey_endpoint = 'https://api.surveymonkey.com/v3/surveys/164317910'
         self.responses = self.fetch_responses()
         self.details = self.fetch_details()
+        self.pages = None
+        self.questions = None
+        self.answers = None
 
     def fetch_responses(self):
         r = requests.get(self.survey_endpoint+'/responses', headers={'Authorization': os.getenv('SURVEY_MONKEY_API_KEY')})
         content = json.loads(r.content)['data']
         ids = [x['id'] for x in content]
         return ids
+
+    def fetch_pages(self):
+        r = requests.get(self.survey_endpoint+'/pages',
+                         headers={'Authorization': os.getenv('SURVEY_MONKEY_API_KEY')})
+        content = json.loads(r.content)['data']
+        self.pages = {(key, value) for (key, value) in zip([x['id'] for x in content],list(range(6))[1:])}
 
     def fetch_details(self):
         r = requests.get(self.survey_endpoint + '/details', headers={'Authorization': os.getenv('SURVEY_MONKEY_API_KEY')})
